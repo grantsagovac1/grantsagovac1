07fc429ffaca7b67976f7c80cd4e43dda029d56e
grantsagovac1 <grantsagovac1@gmail.com>
2022-02-14 20:25:25 -0500

    updatiubng

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def about():
 def Land_Cruiser():
     return render_template("Land_Cruiser.html")
 
+@app.route('/about_me/')
+def about_me():
+    return render_template("about_me.html")
+
 
 if __name__=="__main__":
     app.run(debug=True)
